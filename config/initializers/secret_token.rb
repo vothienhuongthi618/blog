10e67153740f2b9abe9797fe531e10c2c7e0e427
Thien Huong Thi Vo <vo.thienhuongthi618@gmail.com>
2013-03-25 14:51:25 -0700

    Basic Structure
    Created a post with the 7 attributes (index, new, show, edit, destroy,
    create, update)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '10613ee8b2b8c05847f147f00ee0d0562bf1a8f9c13c0147e24fcdb25570d1500abea459f67fd82028bbb103d901438730e4962fde47b5d8a9aef02e63b757a6'
